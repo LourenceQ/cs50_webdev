1eae3b21e667cd425d75017ef9a71b86fc8edd39
lou <lqfmecatronica@gmail.com>
2021-02-08 21:32:10 -0300

    models

--- project_models/airline/flights/views.py
+++ project_models/airline/flights/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from . models import Flight
 # Create your views here.
+def index(request):
+    return render(request,"flights/index.html",
+    {
+    "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request,"flights/flight.html",{
+    "flight": flight,
+    "passengers": flight.passengers.all()
+    })
